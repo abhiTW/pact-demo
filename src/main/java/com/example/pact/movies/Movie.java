e449e465a8611b6cf72bfce59497ac63ee2cbdc9
abhiTW <abhinaym@thoughtworks.com>
2020-07-10 16:51:46 +0530

    [Niranjani] #spike Add. add getter and setter for id attribute

--- src/main/java/com/example/pact/movies/Movie.java
+++ src/main/java/com/example/pact/movies/Movie.java
@@ -17,6 +17,14 @@ public class Movie {
         this.genre = genre;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
